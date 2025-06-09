ad02436ed977b4eb275d5f1de19e630f2e80f1c1
Coderlaraman <coderlaraman@gmail.com>
2025-06-09 15:41:24 -0500

    Página de inicio de Oidivi Helper Mobile 1

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+import './global.css';
+
+export default function App() {
+  return (
+    <View>
+      <Text className="flex justify-center font-bold text-red-500 text-xl">
+        Hello Coderman Hi!
+      </Text>
+    </View>
+  );
+}
